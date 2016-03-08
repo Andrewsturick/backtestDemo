59a250eca96e620c5e2df56730cd57a4a6c3c063
ayushjswl3@gmail.com <ayush513>
2016-03-08 13:11:14 +0530

    changes

--- source/src/app/pt/Admin/Instructor_info/instructorDetails.controller.js
+++ source/src/app/pt/Admin/Instructor_info/instructorDetails.controller.js
@@ -6,46 +6,29 @@
         .controller('InstructorDetailsController', fnCtrl);
 
     /* @ngInject */
-    function fnCtrl( $stateParams,$mdDialog, instructorService,$state,breezeService) {
+    function fnCtrl( $stateParams, instructorService,$state,breezeService) {
         var vm = this;
-        vm.closeItem = closeItem;
+        //vm.closeItem = closeItem;
         vm.deleteEmail = deleteEmail;
-        vm.composeClick=composeClick;
+        vm.backbutton=backbutton;
+        vm.Edit_instructor = Edit_instructor;
 
+        function Edit_instructor()
+        {
+            $state.go("triangular-no-scroll.admin-default-no-scroll.instructor.insEdit",{id:23});
+        }
         vm.item = instructorService.getDetail($stateParams.id)[0];
 
-        function closeItem()
-        {
+
+
+        function backbutton() {
             $state.go("triangular-no-scroll.admin-default-no-scroll.instructor");
         }
 
-        function deleteEmail()
-        {
+        function deleteEmail() {
             $scope.$emit('deleteEmail');
         }
-        function composeClick()
-        {
-            $mdDialog.show({
-                controller: 'instructor_dialogController',
-                controllerAs: 'vm',
-                templateUrl: 'app/pt/Admin/instructor_info/instructor_dialog.tmpl.html',
-            })
-        }
 
-        vm.columns = [{
-            title: '',
-            field: 'thumb',
-            filter: 'tableImage'
-        },{
-            title: 'Name',
-            field: 'firstName'
-        },{
-            title: 'contact no',
-            field: 'contactNo'
-        },{
-            title: 'City',
-            field: 'City'
-        }];
 
 
         breezeService.getEntities('students').then(function (data) {
--- source/src/app/pt/Admin/seed.config.js
+++ source/src/app/pt/Admin/seed.config.js
@@ -12,37 +12,37 @@
         $stateProvider
 
             .state('triangular-no-scroll.admin-default-no-scroll.instructor', {
-                url: '/Admin/instructor',
+                url: '/Admin/instructor/',
                 templateUrl: 'app/pt/Admin/instructor_info/instructor.tmpl.html',
                 controller: 'InstructorController',
                 controllerAs: 'vm'
             })
+            //Todo:rename this state to readonly
             .state('triangular-no-scroll.admin-default-no-scroll.instructor.insDetails', {
-                url: '/:id',
+                url: 'readOnly/:id',
                 templateUrl: 'app/pt/Admin/instructor_info/instructorDetails.tmpl.html',
                 controller: 'InstructorDetailsController',
                 controllerAs: 'vm'
             })
-            .state('triangular-no-scroll.admin-default-no-scroll.instructor.insDetails.dialog', {
-                url: '/:id',
-                templateUrl: 'app/pt/Admin/instructor_info/instructor_dialog.tmpl.html',
-                controller: 'instructor_dialogController',
+            .state('triangular-no-scroll.admin-default-no-scroll.instructor.insEdit', {
+                url: 'edit/:id',
+                templateUrl: 'app/pt/Admin/instructor_info/instructorEdit.tmpl.html',
+                controller: 'InstructorEditController',
                 controllerAs: 'vm'
             })
 
-            .state('triangular-no-scroll.admin-default-no-scroll.location', {
-                url: '/Admin/location',
-                templateUrl: 'app/pt/Admin/Location_info/location.tmpl.html',
-                controller: 'LocationController',
-                controllerAs: 'vm'
+
+
+
+            .state('triangular.admin-default.student_information', {
+                url: '/Admin/blank2',
+                templateUrl: 'app/pt/Admin/blank2.tmpl.html'
             })
 
-                    .state('triangular-no-scroll.admin-default-no-scroll.timeslot', {
-                        url: '/Admin/timeslot',
-                        templateUrl: 'app/pt/Admin/Timeslot_info/timeslot.tmpl.html',
-                        controller: 'TimeslotController',
-                        controllerAs: 'vm'
-                    })
+            .state('triangular.admin-default.new_information', {
+                url: '/Admin/blank3',
+                templateUrl: 'app/pt/Admin/blank3.tmpl.html'
+            });
 
 
         //triMenuProvider.addMenu({
